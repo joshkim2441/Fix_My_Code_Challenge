b60574b5052d5d81251d6deec3cb3da647c5870a
joshkim2441 <joshkim2441@gmail.com>
2024-04-07 09:15:37 +0300

    fix my code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,7 +7,7 @@ class Square():
     width = 0
     height = 0
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """ Initialize the Square class """
         for key, value in kwargs.items():
             setattr(self, key, value)
